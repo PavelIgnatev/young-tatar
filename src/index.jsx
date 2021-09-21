122acbc4966536ff1469e6c35672b787ebd5433a
PavelIgnatev <58823040+PavelIgnatev@users.noreply.github.com>
2021-09-21 13:35:17 +0300

    Добавил сбор статистики

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './scss/index.scss';
+import './statistics/collectingStatistics'
+import './statistics/viewingStatistics'
 
 ReactDOM.render(
   <React.StrictMode>
